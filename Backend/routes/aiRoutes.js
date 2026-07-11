d38d9eac485772d8e867898900c1446aca9d104a
Divya <dasdivya589@gmail.com>
2026-07-12 01:09:00 +0530

    Merge conflicts

--- Backend/routes/aiRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { handleAsk } = require("../controllers/aiController");
-
-router.post("/ask", handleAsk);
-
-module.exports = router;
